5046ffef8d7f72daa5a664903f5f8473674a0556
Konstantin Polyakov <shyzik_93@mail.ru>
2018-10-10 13:38:48 +0300

    Translating in ingliush was added

---
+++ languages/EN.py
@@ -0,0 +1,42 @@
+context = {
+    'product_name': 'Реле WI-FI',
+    'product_version': '0.3',
+    'panel_std': 'Стандарт',
+    'panel_set': 'Settings',
+    'panel_panel': 'Panel',
+    'panel_schedule': 'Schedule',
+
+    'statistic': 'Statistic', 
+    'time_of_job': 'Job\'s time', 
+    'rtc_module': 'Current time of module',
+    'rtc_browser': 'Current time of browser',
+    'voltage': 'Voltage', 
+
+    'set_wifi_mode': 'Mode', 
+    'set_wifi_mode_client': 'Client', 
+    'set_wifi_mode_ap': 'Access point', 
+    'set_wifi_mode_both': 'Both',
+
+    'set_device_name': 'Device name', 
+    'set_wifi': 'Settings WI-FI', 
+    'set_wifi_ssid': 'Network name', 
+    'set_wifi_password': 'Password', 
+
+    'set_time': 'Real time clock', 
+    'set_time_date': 'Date', 
+    'set_time_time': 'Time', 
+    'set_time_set_from_browser': 'Get date/time from your browser and save it', 
+
+    'set_other': 'Other', 
+    'set_other_update_time': 'Time period to update this page', 
+    'set_other_reset': 'Reset the settings', 
+    'set_other_reboot': 'Reboot',
+    'set_other_reset_question': 'The settings will be reset irrevocably. Continue?', 
+    'set_other_reset_done': 'The settings has been reset. Please, reboot the device.', 
+    'set_other_reboot_question': 'The device will reboot. Continue?', 
+    'set_other_reboot_done': 'The device has been rebooted. Please, reload this page after several seconds.', 
+
+    'save': 'Save',
+
+    '': '', 
+}
---
+++ languages/RU.py
@@ -0,0 +1,42 @@
+context = {
+    'product_name': 'Реле WI-FI',
+    'product_version': '0.3',
+    'panel_std': 'Стандарт',
+    'panel_set': 'Настройки',
+    'panel_panel': 'Панель',
+    'panel_schedule': 'Расписание',
+
+    'statistic': 'Статистика', 
+    'time_of_job': 'Время работы', 
+    'rtc_module': 'Текущее время модуля',
+    'rtc_browser': 'Текущее время браузера',
+    'voltage': 'Напряжение', 
+
+    'set_wifi_mode': 'Режим работы', 
+    'set_wifi_mode_client': 'Клиент', 
+    'set_wifi_mode_ap': 'Точка доступа', 
+    'set_wifi_mode_both': 'Оба варианта',
+
+    'set_device_name': 'Имя устройства', 
+    'set_wifi': 'Настройки WI-FI', 
+    'set_wifi_ssid': 'Имя сети', 
+    'set_wifi_password': 'Пароль', 
+
+    'set_time': 'Настройки часов', 
+    'set_time_date': 'Дата', 
+    'set_time_time': 'Время', 
+    'set_time_set_from_browser': 'Взять время из браузера и сохранить', 
+
+    'set_other': 'Прочее', 
+    'set_other_update_time': 'Период обновления страницы', 
+    'set_other_reset': 'Сбросить настройки', 
+    'set_other_reboot': 'Перезапустить', 
+    'set_other_reset_question': 'Настройки будут безвозвратно сброшены на заводские. Продолжить?', 
+    'set_other_reset_done': 'Настройки сброшены. Перезагрузите устройство.', 
+    'set_other_reboot_question': 'Устройство будет перезапущено. Продолжить?', 
+    'set_other_reboot_done': 'Устройство перезапущено. Через несколько секунд обновите данную страницу.', 
+
+    'save': 'Сохранить',
+    
+     '': '', 
+}
